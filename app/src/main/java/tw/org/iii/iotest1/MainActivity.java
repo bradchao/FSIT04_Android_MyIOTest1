d0cc2f7888a6dbb964273219a9b569cad90ac246
Brad <brad@brad.tw>
2018-03-17 16:06:14 +0800

    v4

--- app/src/main/java/tw/org/iii/iotest1/MainActivity.java
+++ app/src/main/java/tw/org/iii/iotest1/MainActivity.java
@@ -1,7 +1,12 @@
 package tw.org.iii.iotest1;
 
+import android.Manifest;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -18,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
     private SharedPreferences sp;
     private SharedPreferences.Editor editor;
     private TextView tv;
-    private File sdroot;
+    private File sdroot, approot;
 
 
     @Override
@@ -26,14 +31,50 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tv = findViewById(R.id.tv);
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    123);
+        }else{
+            init();
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (grantResults.length > 0
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            // OK
+            init();
+        }else{
+            // NO
+            finish();
+        }
+
+    }
+
+    private void init(){
+       tv = findViewById(R.id.tv);
         sp = getSharedPreferences("gamedata", MODE_PRIVATE);
         editor = sp.edit();
 
         String state = Environment.getExternalStorageState();
-        Log.v("brad", state);
+        //Log.v("brad", state);
+        sdroot = Environment.getExternalStorageDirectory();
+        Log.v("brad", sdroot.getAbsolutePath());
+
+        approot = new File(sdroot, "Android/data/" + getPackageName());
+        if (!approot.exists()){
+            approot.mkdirs();
+        }
 
     }
+
 
     public void test1(View view) {
         String name = sp.getString("name",
@@ -95,8 +136,69 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void test5(View view) {
+        File file1 = new File(sdroot, "brad.txt");
+        try {
+            FileOutputStream fout = new FileOutputStream(file1);
+            fout.write("Hello, Brad".getBytes());
+            fout.flush();
+            fout.close();
+            Toast.makeText(this,
+                    "Save OK3", Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            Log.v("brad", e.toString());
+        }
     }
 
     public void test6(View view) {
+        File file1 = new File(approot, "brad.txt");
+        try {
+            FileOutputStream fout = new FileOutputStream(file1);
+            fout.write("Hello, Brad".getBytes());
+            fout.flush();
+            fout.close();
+            Toast.makeText(this,
+                    "Save OK4", Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            Log.v("brad", e.toString());
+        }
+    }
+
+    public void test7(View view) {
+        File file1 = new File(sdroot, "brad.txt");
+        if (file1.exists()){
+            try {
+                FileInputStream fileInputStream =
+                        new FileInputStream(file1);
+                byte[] buf = new byte[(int)file1.length()];
+                fileInputStream.read(buf);
+                fileInputStream.close();
+                Toast.makeText(this,
+                        new String(buf), Toast.LENGTH_SHORT).show();
+            }catch (Exception e){
+                Log.v("brad", e.toString());
+            }
+        }else{
+            Toast.makeText(this,
+                    "not exist", Toast.LENGTH_SHORT).show();
+        }
+    }
+    public void test8(View view) {
+        File file1 = new File(approot, "brad.txt");
+        if (file1.exists()){
+            try {
+                FileInputStream fileInputStream =
+                        new FileInputStream(file1);
+                byte[] buf = new byte[(int)file1.length()];
+                fileInputStream.read(buf);
+                fileInputStream.close();
+                Toast.makeText(this,
+                        new String(buf), Toast.LENGTH_SHORT).show();
+            }catch (Exception e){
+                Log.v("brad", e.toString());
+            }
+        }else{
+            Toast.makeText(this,
+                    "not exist", Toast.LENGTH_SHORT).show();
+        }
     }
 }
